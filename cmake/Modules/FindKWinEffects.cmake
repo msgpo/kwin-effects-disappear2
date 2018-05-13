3962622dd4406291d8f8241f6545fdd9564ffe7f
Vlad Zagorodniy <vladzzag@gmail.com>
2018-05-14 00:12:15 +0300

    Fix perspective distortions
    Perspective projection distorts objects near edges of the viewport too
    much. Fix this problem by moving the center of windows to origin.

--- cmake/Modules/FindKWinEffects.cmake
+++ cmake/Modules/FindKWinEffects.cmake
@@ -4,31 +4,53 @@
 #  KWINEFFECTS_FOUND - System has kwineffects
 #  KWINEFFECTS_INCLUDE_DIRS - The kwineffects include directories
 #  KWINEFFECTS_LIBRARIES - The libraries needed to use kwineffects
+#  KWINEFFECTS_GLUTILS_INCLUDE_DIRS - The kwinglutils include directories
+#  KWINEFFECTS_GLUTILS_LIBRARIES - The libraries needed to use kwinglutils
 #
 # Also, once done this will define the following targets
 #  KWinEffects::KWinEffects
+#  KWinEffects::GLUtils
 #
 
 find_path (KWINEFFECTS_INCLUDE_DIRS
     NAMES kwineffects.h
 )
 
+find_path (KWINEFFECTS_GLUTILS_INCLUDE_DIRS
+    NAMES kwinglutils.h
+)
+
 find_library (KWINEFFECTS_LIBRARY
     NAMES kwineffects
+)
+
+find_library (KWINEFFECTS_GLUTILS_LIBRARY
+    NAMES kwinglutils
 )
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (KWinEffects DEFAULT_MSG
     KWINEFFECTS_LIBRARY KWINEFFECTS_INCLUDE_DIRS
+    KWINEFFECTS_GLUTILS_LIBRARY KWINEFFECTS_GLUTILS_INCLUDE_DIRS
 )
-mark_as_advanced (KWINEFFECTS_LIBRARY)
+mark_as_advanced (
+    KWINEFFECTS_LIBRARY
+    KWINEFFECTS_GLUTILS_LIBRARY
+)
 
 set (KWINEFFECTS_LIBRARIES ${KWINEFFECTS_LIBRARY})
+set (KWINEFFECTS_GLUTILS_LIBRARIES ${KWINEFFECTS_GLUTILS_LIBRARY})
 
 if (KWINEFFECTS_FOUND)
     add_library (KWinEffects::KWinEffects UNKNOWN IMPORTED)
     set_target_properties (KWinEffects::KWinEffects PROPERTIES
         IMPORTED_LOCATION "${KWINEFFECTS_LIBRARY}"
         INTERFACE_INCLUDE_DIRECTORIES "${KWINEFFECTS_INCLUDE_DIRS}"
+    )
+
+    add_library (KWinEffects::GLUtils UNKNOWN IMPORTED)
+    set_target_properties (KWinEffects::GLUtils PROPERTIES
+        IMPORTED_LOCATION "${KWINEFFECTS_GLUTILS_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${KWINEFFECTS_GLUTILS_INCLUDE_DIRS}"
     )
 endif ()
--- src/Disappear2Effect.cc
+++ src/Disappear2Effect.cc
@@ -20,6 +20,12 @@
 // KConfigSkeleton
 #include "disappear2config.h"
 
+// kwineffects
+#include <kwinglutils.h>
+
+// Qt
+#include <QMatrix4x4>
+
 namespace {
 const int Disappear2WindowRole = 0x22A98400;
 }
@@ -92,6 +98,28 @@ void Disappear2Effect::paintWindow(KWin::EffectWindow* w, int mask, QRegion regi
     const auto it = m_animations.constFind(w);
     if (it != m_animations.cend()) {
         const qreal t = (*it).value();
+
+        const QRectF screenGeo = KWin::GLRenderTarget::virtualScreenGeometry();
+        const QRectF windowGeo = w->geometry();
+
+        // Perspective projection distorts objects near edges
+        // of the viewport. This is critical because distortions
+        // near edges of the viewport are not desired with this effect.
+        // To fix this, the center of the window will be moved to the origin,
+        // after applying perspective projection, the center is moved back
+        // to its "original" projected position. Overall, this is how the window
+        // will be transformed:
+        //  [move to the origin] -> [rotate] -> [translate] ->
+        //    -> [perspective projection] -> [reverse "move to the origin"]
+        const QMatrix4x4 oldProjMatrix = data.screenProjectionMatrix();
+        const QVector3D invOffset = oldProjMatrix.map(QVector3D(windowGeo.center()));
+        QMatrix4x4 invOffsetMatrix;
+        invOffsetMatrix.translate(invOffset.x(), invOffset.y());
+        data.setProjectionMatrix(invOffsetMatrix * oldProjMatrix);
+
+        // Move the center of the window to the origin.
+        const QPointF offset = screenGeo.center() - windowGeo.center();
+        data.translate(offset.x(), offset.y());
 
         data.setRotationAxis(Qt::XAxis);
         data.setRotationOrigin(QVector3D(0, w->height(), 0));
